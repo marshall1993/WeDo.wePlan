a08aa997aba62f0a4df386fa0becab882c92c408
MarshallJin <marshal.jin@outlook.com>
2016-04-26 10:37:25 +0800

    add base maven project
    new

---
+++ weSculpture-cross-business-service-api/src/main/java/WeDo/weSculpture/weSculpture_cross_business_service_api/App.java
@@ -0,0 +1,13 @@
+package WeDo.weSculpture.weSculpture_cross_business_service_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ weSculpture-prime-service-impl/src/main/java/WeDo/weSculpture/weSculpture_prime_service_impl/App.java
@@ -0,0 +1,13 @@
+package WeDo.weSculpture.weSculpture_prime_service_impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
